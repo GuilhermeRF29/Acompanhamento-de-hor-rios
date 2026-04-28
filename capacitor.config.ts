6a5766e5cc323e067bbad4ecd5fd332973f80eef
Guilherme Rodrigues <49718109+GuilhermeRF29@users.noreply.github.com>
2026-04-28 14:51:12 -0300

    Instalação do projeto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.controle.partidas',
+  appName: ' Controle de Partidas',
+  webDir: 'dist'
+};
+
+export default config;
